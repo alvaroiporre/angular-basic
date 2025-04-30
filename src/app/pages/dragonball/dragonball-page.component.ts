6687bf7f6ef2869abb1669cf039b9b1b19e77d00
alvaroiporre <alvaroiporremartinez@gmail.com>
2025-04-29 21:02:40 -0400

    Add use of inputs
    Signed-off-by: alvaroiporre <alvaroiporremartinez@gmail.com>

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -12,27 +12,30 @@ interface Character {
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
     {
       id: 1,
       name: 'Goku',
-      power: 9001
+      power: 9001,
     },
     {
       id: 2,
       name: 'Vegeta',
-      power: 8000
+      power: 8000,
     },
     {
       id: 3,
       name: 'Piccolo',
-      power: 3000
+      power: 3000,
     },
     {
       id: 4,
       name: 'Yamcha',
-      power: 500
-    }
+      power: 500,
+    },
   ]);
 
   powerClasses = computed(() => {
@@ -40,4 +43,25 @@ export class DragonballPageComponent {
       'text-danger': true,
     };
   });
+
+  addCharacter() {
+    if (!this.name() || !this.power || this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters.length,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((current) => [...current, newCharacter]);
+    // this.characters().push(newCharacter);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
